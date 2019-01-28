3141a31751a71c3051e90a8950dcd344cb02850c
samiu980728 <39590937+samiu980728@users.noreply.github.com>
2019-01-28 20:57:18 +0800

    Add files via upload

---
+++ XWSacnBigImage-master/README.md
@@ -0,0 +1,29 @@
+# XWSacnBigImage
+点击图片查看大图，再次点击还原原始尺寸
+
+<br>显示效果 ＜/br＞
+
+![](https://github.com/qxuewei/XWSacnBigImage/raw/master/GIF/XWScanImageGif.gif)  
+
+---
+使用方法：
+</code></pre>
+   <br> 1.导入XWScanImageClass ＜/br＞
+   <br> 2.为UIImageView增加点击事件，并且开始用户交互属性＜/br＞
+<code><pre>
+// - 浏览大图点击事件
+    //为UIImageView1添加点击事件
+    UITapGestureRecognizer *tapGestureRecognizer1 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scanBigImageClick1:)];
+    [_ImageView1 addGestureRecognizer:tapGestureRecognizer1];
+    //让UIImageView和它的父类开启用户交互属性
+    [_ImageView1 setUserInteractionEnabled:YES];
+</code></pre>
+    3.在监听事件方法中调用类方法 scanBigImageWithImageView
+<code><pre>
+// - 浏览大图点击事件
+    -(void)scanBigImageClick1:(UITapGestureRecognizer *)tap{
+    NSLog(@"点击图片");
+    UIImageView *clickedImageView = (UIImageView *)tap.view;
+    [XWScanImage scanBigImageWithImageView:clickedImageView];
+    }
+</code></pre>
---
+++ XWSacnBigImage-master/XWScanImageDemo/XWScanImageDemo/ViewController.m
@@ -0,0 +1,75 @@
+//
+//  ViewController.m
+//  XWScanImageDemo
+//
+//  Created by 邱学伟 on 16/4/14.
+//  Copyright © 2016年 邱学伟. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "XWScanImage.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *ImageView1;
+
+@property (weak, nonatomic) IBOutlet UIImageView *ImageView2;
+
+@property (weak, nonatomic) IBOutlet UIImageView *ImageView3;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //为UIImageView1添加点击事件
+    UITapGestureRecognizer *tapGestureRecognizer1 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scanBigImageClick1:)];
+    [_ImageView1 addGestureRecognizer:tapGestureRecognizer1];
+    //让UIImageView和它的父类开启用户交互属性
+    [_ImageView1 setUserInteractionEnabled:YES];
+    
+    
+    //为UIImageView2添加点击事件
+    UITapGestureRecognizer *tapGestureRecognizer2 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scanBigImageClick2:)];
+    [_ImageView2 addGestureRecognizer:tapGestureRecognizer2];
+    [_ImageView2 setUserInteractionEnabled:YES];
+    
+    //为UIImageView3添加点击事件
+    UITapGestureRecognizer *tapGestureRecognizer3 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scanBigImageClick3:)];
+    [_ImageView3 addGestureRecognizer:tapGestureRecognizer3];
+    [_ImageView3 setUserInteractionEnabled:YES];
+
+}
+
+#pragma mark - 浏览大图点击事件
+-(void)scanBigImageClick1:(UITapGestureRecognizer *)tap{
+    NSLog(@"点击图片1");
+    UIImageView *clickedImageView = (UIImageView *)tap.view;
+    [XWScanImage scanBigImageWithImageView:clickedImageView];
+}
+
+-(void)scanBigImageClick2:(UITapGestureRecognizer *)tap{
+    NSLog(@"点击图片2");
+    UIImageView *clickedImageView = (UIImageView *)tap.view;
+    
+    [XWScanImage scanBigImageWithImageView:clickedImageView];
+    
+}
+-(void)scanBigImageClick3:(UITapGestureRecognizer *)tap{
+    NSLog(@"点击图片3");
+    UIImageView *clickedImageView = (UIImageView *)tap.view;
+    
+    UIWindow *window = [UIApplication sharedApplication].keyWindow;
+    [XWScanImage scanBigImageWithImage:clickedImageView.image frame:[clickedImageView convertRect:clickedImageView.bounds toView:window]];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
